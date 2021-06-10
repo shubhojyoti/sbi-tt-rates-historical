d69a2214a4793b449be3e07936d7dd1b011b9169
Shubhojyoti Bhattacharya (auto commit) <shubhojyoti.b@gmail.com>
2021-06-10 19:35:02 +0530

    Daily update 2021-06-10-19:35

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 wget https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf -O ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
 git add .
-git config user.email "noreply@shivamkhandelwal.in"
-git config user.name "Shivam Khandelwal (auto commit)"
+git config user.email "shubhojyoti.b@gmail.com"
+git config user.name "Shubhojyoti Bhattacharya (auto commit)"
 git commit -m "Daily update $(date '+%Y-%m-%d-%H:%M')"
 git push origin master
