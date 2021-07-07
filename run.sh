d8491664cc821a821dbcaadf973be8b75c7143c1
Shubhojyoti Bhattacharya (auto commit) <shubhojyoti.b@gmail.com>
2021-07-07 17:48:53 +0200

    Daily update 2021-07-07-17:48

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-wget https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf -O ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
+wget https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf -O ~/sbi-tt-rates-historical/$(TZ='Asia/Calcutta' date '+%Y-%m-%d-%H:%M').pdf
 git add .
 git config user.email "shubhojyoti.b@gmail.com"
 git config user.name "Shubhojyoti Bhattacharya (auto commit)"
